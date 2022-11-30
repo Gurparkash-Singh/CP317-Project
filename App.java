f506beeb35b6960a051210cc81657c8ebbce305f
Gurparkash Randhawa <gurparkashrandhawa@Gurparkashs-MacBook-Pro.local>
2022-11-30 14:33:35 -0500

    Added test to block

--- App.java
+++ App.java
@@ -1,13 +1,15 @@
 public class App {
     public static void main(String[] args) {
         // For testing purposes only
-        Student saara = new Student("Saara Beattie", 237711821);
-        saara.addCourse("PS262", new float[]{56, 96, 75, 95});
-        saara.addCourse("MA370", new float[]{57, 75, 55, 73});
-        String[] results = saara.formattedCourses();
-        for (String result : results)
         {
-            System.out.println(result);
+            Student saara = new Student("Saara Beattie", 237711821);
+            saara.addCourse("PS262", new float[]{56, 96, 75, 95});
+            saara.addCourse("MA370", new float[]{57, 75, 55, 73});
+            String[] results = saara.formattedCourses();
+            for (String result : results)
+            {
+                System.out.println(result);
+            }
         }
     }
 }
